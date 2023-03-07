7b12b63deda9e9642eaf604a12eb65c6a4b24ac7
Abdalkarim Shehab <abdo.r.shehab2@gmail.com>
2023-03-07 13:02:28 +0200

    Attendance Working Hours Report Added

--- erpnext_gsg/erpnext_gsg/report/attendance_working_hours/attendance_working_hours.js
+++ erpnext_gsg/erpnext_gsg/report/attendance_working_hours/attendance_working_hours.js
@@ -4,9 +4,25 @@
 
 frappe.query_reports["Attendance Working Hours"] = {
 	"filters": [
-		{'fieldname': 'from_date', 'label': 'From Date', 'fieldtype': 'Date'},
-		{'fieldname': 'to_date', 'label': 'To Date', 'fieldtype': 'Date'},
-		{'fieldname': 'employee', 'label': 'Employee', 'fieldtype': 'Link', 'options': 'Employee'},
-		{'fieldname': 'department', 'label': 'Department', 'fieldtype': 'Link', 'options': 'Department'}
+		{'fieldname': 'from_date',
+		 'label': 'From Date',
+		  'fieldtype': 'Date',
+		  'default': frappe.datetime.add_months(frappe.datetime.get_today(), -1)
+		  },
+		{'fieldname': 'to_date',
+		 'label': 'To Date',
+		 'fieldtype': 'Date',
+         'default': frappe.datetime.get_today()
+         },
+		{'fieldname': 'employee',
+		'label': 'Employee',
+		'fieldtype': 'Link',
+		'options': 'Employee',
+		},
+		{'fieldname': 'department',
+		'label': 'Department',
+		'fieldtype': 'Link',
+		'options': 'Department',
+		}
 	]
 };
--- erpnext_gsg/erpnext_gsg/report/attendance_working_hours/attendance_working_hours.py
+++ erpnext_gsg/erpnext_gsg/report/attendance_working_hours/attendance_working_hours.py
@@ -2,26 +2,28 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.utils import time_diff
 
 
 def execute(filters=None):
     columns, data = [], []
-    columns = get_columns(filters)
-    data = get_data(filters)
+    columns = get_columns()
+    conditions = get_conditions(filters)
+    data = get_data(conditions, filters)
     return columns, data
 
 
 def get_conditions(filters):
     conditions = ""
-    if filters.get("from_date"):
-        conditions += " and attendance_date > %(from_date)s"
-    if filters.get("to_date"):
-        conditions += " and attendance_date < %(to_date)s"
-
+    if filters:
+        if filters.get("from_date"):
+            conditions += "and attendance_date > %(from_date)s "
+        if filters.get("to_date"):
+            conditions += "and attendance_date < %(to_date)s"
+        if filters.get("employee"):
+            conditions += "and employee = %(employee)s"
+        if filters.get("department"):
+            conditions += "and department = %(department)s"
     return conditions
-
-
 
 
 def get_columns():
@@ -32,10 +34,19 @@ def get_columns():
         {'fieldname': 'check_in', 'label': 'Check In', 'fieldtype': 'Time'},
         {'fieldname': 'check_out', 'label': 'Check Out', 'fieldtype': 'Time'},
         {'fieldname': 'working_hours', 'label': 'Working Hours', 'fieldtype': 'Float'},
+        {'fieldname': 'view', 'label': 'View', 'fieldtype': 'HTML',
+         'options': '<a target="_blank" href="#">view</a>'},
     ]
     return columns
 
 
-def get_data(filters):
-    return frappe.db.get_all('Attendance', ['employee', 'employee_name', 'attendance_date', 'check_in', 'check_out',
-                                            'time(check_out-check_in) as working_hours'], filters=filters)
+def get_data(conditions, filters):
+    data = frappe.db.sql(
+        """SELECT name as view, employee, employee_name, attendance_date, check_in, check_out,
+         CASE WHEN check_in and check_out THEN time(check_out-check_in) ELSE 0 END as 
+        working_hours FROM `tabAttendance` WHERE docstatus = 1 {conditions}""".format(conditions=conditions), filters,
+        as_dict=1)
+    for x in data:
+        x["view"] = f""" <a target="_blank" href="#" 
+        onclick = 'window.open("http://0.0.0.0:8000/app/attendance/{x["view"]}");return false;'>view</a>"""
+    return data
